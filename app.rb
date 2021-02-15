61e9b3796049d7c72d568b6534ce4b074e1991d5
Eds <steadyede@live.co.uk>
2021-02-15 16:22:29 +0000

    Picture functionality

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+configure(:development) { set :session_secret, "something" }
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Joe Wicks likes bacon."
+end
+
+get '/eds' do
+  "Big Eds is here"
+end
+
+get '/trophy' do
+  "<div style='border: 3px dashed red'>
+     <img src='https://i.redd.it/v35cg8cr6i521.jpg'>
+   </div>"
+end
+ 
+
+get '/games' do
+  "No time for games."
 end
